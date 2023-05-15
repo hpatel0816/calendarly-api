9cb3b986b55ed7841ea2cfb78b14eca673e11b70
Harsh <harshpatel1632@gmail.com>
2023-05-15 17:45:13 -0400

    Minor change to dockerfile and removing secrets file

--- security/secrets.py
+++
@@ -1,6 +0,0 @@
-#Secret key for flask app
-secretKey = "63202f67298ba35740059f9c23c65738e2fa8bf5a573f40baa51bce476a7e730"
-
-#Database secrets
-password = "91NIkswPxoRhRfSH"
-databaseURI = f"mongodb+srv://hp16:{password}@app-db.26uz4cl.mongodb.net/?retryWrites=true&w=majority"
